62cc321a83f950f0803296f1d51a9c894eebf81e
Michael Benshoof <michael.benshoof@percona.com>
2018-10-29 17:05:33 -0500

    Doing something in mysql

--- hello-test/dbtest.php
+++ hello-test/dbtest.php
@@ -11,5 +11,30 @@ if ($connection->connect_errno) {
 } else {
     printf("Connected to the database - and webhooks are working now :)");
 }
+
+/* Select queries return a resultset */
+if ($result = $mysqli->query("SELECT * FROM mytable")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+
+    dar($result);
+
+
+    while ($row = $result->fetch_object()){
+       dar($row);
+    }
+
+    /* free result set */
+    $result->close();
+}
+
 $connection->close();
+
+
+function dar($data) {
+
+	echo "<pre>";
+	var_dump($data);
+	echo "</pre>";
+}
+
 ?>
