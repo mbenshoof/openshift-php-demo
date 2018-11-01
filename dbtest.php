b51a8bca51d0acf09ec40ad57a339e8f3fcecbc6
Michael Benshoof <michael.benshoof@percona.com>
2018-11-01 11:40:57 -0500

    Formatting fixes

--- dbtest.php
+++ dbtest.php
@@ -5,26 +5,24 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-echo "<i>Attempting to connect to: $dbhost ... <br /><br />"; 
+echo "<i>Attempting to connect to: $dbhost ... </i><br /><br />"; 
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
-    printf("Connected to the database - and webhooks are working now :)");
+    echo "Connected to the database - and webhooks are working now :) <br /><br />";
 }
 
 /* Select queries return a resultset */
 if ($result = $connection->query("SELECT * FROM mytable")) {
-    //printf("Select returned %d rows.\n", $result->num_rows);
 
     
     echo "<p><strong>Num Rows:</strong> " . $result->num_rows . "</strong></p>";
     echo "<ul>";
 
     while ($row = $result->fetch_object()){
-       //dar($row);
        echo "<li>" . $row->id . " : " . $row->val . "</li>";
     }
 
